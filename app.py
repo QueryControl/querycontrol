1e487dc97c1a06cfd1fcd6c41c7c7dd23e27b7fc
Tim Clemans <tim@insideyourgovernment.com>
2016-05-04 23:36:48 +0000

    try fixing port issue

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ def page_not_found(error):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', debug=True, port=port)
