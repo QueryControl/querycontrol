1195b8354d361e476870e583e5c60de2ae56a4d1
Tim Clemans <tim@insideyourgovernment.com>
2016-05-04 23:04:49 +0000

    add basic code for QueryControl

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ This file creates your application.
 
 import os
 from flask import Flask, render_template, request, redirect, url_for
-
+import flask
 app = Flask(__name__)
-
+from flask_cors import *
+import requests
+import re
+import json
+import urllib
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
 
@@ -29,27 +33,86 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+import requests
+import re
 
-###
-# The functions below should be applicable to all Flask apps.
-###
+def validate_url(url):
+    if not "$select" in url:
+        return {"error": "$select required"}
+    if "*" in url:
+        return {"error": "* not allowed"}
+    return
 
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
+def parse_url(url):
+    if ":total_count" in url:
+        if not "$group" in url:
+            url = url.replace(":total_count", "count(*)")
+        else:
+            count_url = url[:url.find('.json')+5] + '?$select=count(*) as count'
+            total_count = requests.get(count_url).json()[0]['count']
+            url = url.replace(":total_count", total_count)
+    m = re.search(',:group_count WHERE \((?P<where_expression>.*?)\)/:group_count as (?P<as>[a-zA-Z]+)[,&]', urllib.unquote(url))
+    if m:
+        print m.group('where_expression')
+        modified_url = urllib.unquote(url).replace(m.group()[:-1], ',count(*) as [REPLACEWITHAS]').replace('%2F', '/')
+        if '$order' in modified_url:
+            
+            modified_url = modified_url.replace(re.search('&\$order=.*', modified_url).group(), '')
+        if '$where' in modified_url:
+            print modified_url
+            where_section = re.search("(?P<replace>\$where=.*)[\&]*", modified_url).group('replace')
+            print where_section
+            numerator_url = modified_url.replace(where_section, where_section + ' AND ' + m.group('where_expression'))
+        else:
+            numerator_url = modified_url + '&$where=' + m.group('where_expression')
+        numerator_url = numerator_url.replace('[REPLACEWITHAS]', 'numerator')
+        print numerator_url
+        denominator_url = modified_url.replace('[REPLACEWITHAS]', 'denominator')
+        print denominator_url
+        print "NU", numerator_url
+        numerators = requests.get(numerator_url).json()
+        denominators = requests.get(denominator_url).json()
+        percentages = []
+        for row in numerators:
+            print 'row', row
+            modified_row = row.copy()
+            del modified_row['numerator']
+            for mrow in denominators:
+                match = True
+                for col in modified_row:
+                    if modified_row.get(col) != mrow.get(col):
+                        match = False
+                        break
+                if match:
+                    matching_denominator_row = mrow
+            modified_row['percentage'] = float(row['numerator'])/float(matching_denominator_row['denominator'])
+            percentages.append(modified_row)
+        return sorted(percentages, key=lambda x: x.get('percentage'), reverse=True)
+    if ":group_count" in url:
+        url = url.replace(":group_count", "count(*)")
+    return url
 
-
-@app.after_request
-def add_header(response):
+@app.route('/forsocrata/<domain>/<datasetid>.json/')
+@cross_origin()
+def for_socrata(domain, datasetid):
     """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
+    Do not use count(*). Use :total_count for the count(*) of the total dataset and
+    :group_count for count(*) of a groupping. 
     """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
+    if not "?" in request.url:
+        return json.dumps({"error": "$select required"})
+    url = 'https://%s/resource/%s.json%s' % (domain, datasetid, request.url[request.url.index('?'):])
+    is_error = validate_url(url)
+    if is_error:
+        return is_error
+    url = parse_url(url)
+    print url
+    if isinstance(url, str):
+        return json.dumps(requests.get(url).json())
+    else:
+        return json.dumps(url)
+
+
 
 
 @app.errorhandler(404)
