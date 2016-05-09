556fdfea9e875f313fa5fe5aacc6768f497db664
Tim Clemans <tim@insideyourgovernment.com>
2016-05-09 17:35:06 +0000

    fixed typo

--- app.py
+++ app.py
@@ -282,7 +282,7 @@ def for_socrata_get_fieldnames(domain, datasetid):
 def for_socrata_owned_datasets():
     userid = requests.get("https://%s/api/users/current.json" % (socrata_access_log_domain), auth=HTTPBasicAuth('tim@insideyourgovernment.com', '***REMOVED***')).json()['id']
     datasets = requests.get('https://%s/api/search/views.json?accessType=WEBSITE&limit=10&page=1&sortBy=newest&for_user=%s&nofederate=true&publication_stage%%5B%%5D=published&publication_stage%%5B%%5D=unpublished&id=%s&row_count=3' % (socrata_access_log_domain, userid, userid), auth=HTTPBasicAuth('tim@insideyourgovernment.com', '***REMOVED***')).json()['results']
-    return Response(json.dumps(datasetss), mimetype='application/json')
+    return Response(json.dumps(datasets), mimetype='application/json')
 
 @app.errorhandler(404)
 def page_not_found(error):
