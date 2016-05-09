825e4b0a9b5243c30a1c0e0ef6d048536f8bd2c3
Tim Clemans <tim@insideyourgovernment.com>
2016-05-09 23:18:48 +0000

    remove username/password replace with socrata_username and socrata_password

--- app.py
+++ app.py
@@ -283,8 +283,8 @@ def for_socrata_get_fieldnames(domain, datasetid):
 @app.route('/forsocrata/owned_datasets/')
 @cross_origin()
 def for_socrata_owned_datasets():
-    userid = requests.get("https://%s/api/users/current.json" % (socrata_access_log_domain), auth=HTTPBasicAuth('tim@insideyourgovernment.com', '***REMOVED***')).json()['id']
-    datasets = requests.get('https://%s/api/search/views.json?accessType=WEBSITE&limit=10&page=1&sortBy=newest&for_user=%s&nofederate=true&publication_stage%%5B%%5D=published&publication_stage%%5B%%5D=unpublished&id=%s&row_count=3' % (socrata_access_log_domain, userid, userid), auth=HTTPBasicAuth('tim@insideyourgovernment.com', '***REMOVED***')).json()['results']
+    userid = requests.get("https://%s/api/users/current.json" % (socrata_access_log_domain), auth=HTTPBasicAuth(socrata_username, socrata_password)).json()['id']
+    datasets = requests.get('https://%s/api/search/views.json?accessType=WEBSITE&limit=10&page=1&sortBy=newest&for_user=%s&nofederate=true&publication_stage%%5B%%5D=published&publication_stage%%5B%%5D=unpublished&id=%s&row_count=3' % (socrata_access_log_domain, userid, userid), auth=HTTPBasicAuth(socrata_username, socrata_password)).json()['results']
     datasets = [dataset['view'] for dataset in datasets]
     return Response(json.dumps(datasets), mimetype='application/json')
 
